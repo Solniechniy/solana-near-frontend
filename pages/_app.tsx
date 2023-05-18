96baee21d0604b6d801e13a01fcb6dfe9da3dcb6
Solniechniy <korniichuk.oleksandr@airslate.com>
2023-05-18 11:36:41 +0300

    update meta information

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import WalletContextProvider from "../components/WalletContextProvider"
+import Head from "next/head"
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <WalletContextProvider>
+        <Head>
+          <title>Boca Chica</title>
+        </Head>
         <Component {...pageProps} />
       </WalletContextProvider>
   )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          name="description"
+          content="Boca Chica is the premier IDO platform that leverages the specifics and native infrastructure of each blockchain to deliver tokenless and frictionless fundraising"
+        />
+        <link rel="icon" href="/favicon.jpeg" />
+        <link rel="apple-touch-icon" href="/favicon.jpeg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
